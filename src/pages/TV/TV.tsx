4398d7c7ce6c92986a49e16be6edddb1313db4e8
solo_bog <bsoloxa@gmail.com>
2021-07-20 12:35:59 +0300

    connect tv page with store

--- src/pages/TV/TV.tsx
+++ src/pages/TV/TV.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PageHeader from "../../components/PageHeader/PageHeader";
+import {
+  fetchData,
+  selectAiringToday,
+  selectOnTV,
+  selectPopular,
+  selectTopRated,
+} from "../../features/tv/tvSlice";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import PageSection from "../../components/PageSection/PageSection";
 
 const Tv = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+  const popular = useAppSelector(selectPopular);
+  const onTV = useAppSelector(selectOnTV);
+  const airingToday = useAppSelector(selectAiringToday);
+  const topRated = useAppSelector(selectTopRated);
   return (
     <>
       <PageHeader title="TV" />
+      <PageSection title="Popular" link="/tv/popular" items={popular} />
+      <PageSection title="On TV" link="/tv/on-tv" items={onTV} />
+      <PageSection
+        title="Airing Today"
+        link="/tv/airing-today"
+        items={airingToday}
+      />
+      <PageSection title="Top Rated" link="/tv/top-rated" items={topRated} />
     </>
   );
 };
