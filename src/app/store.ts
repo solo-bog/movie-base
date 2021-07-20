e2fe19dee657d42ba9f8b6f3cd7c008b7db8a462
solo_bog <bsoloxa@gmail.com>
2021-07-20 17:33:22 +0300

    delete counter reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,13 +2,10 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 // eslint-disable-next-line import/no-cycle
 import moviesReducer from "../features/movies/moviesSlice";
 // eslint-disable-next-line import/no-cycle
-import counterReducer from "../features/counter/counterSlice";
-// eslint-disable-next-line import/no-cycle
 import tvReducer from "../features/tv/tvSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     movies: moviesReducer,
     tv: tvReducer,
   },
