09550581f57fb58658c730d8495dcda151582287
solo_bog <bsoloxa@gmail.com>
2021-07-22 18:47:35 +0300

    add ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import MainRouter from "./components/MainRouter/MainRouter";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
 const App: React.FC = () => {
   return (
-    <Switch>
-      <Route path="/movies/:id">Movie</Route>
-      <Route path="/">
-        <MainRouter />
-      </Route>
-    </Switch>
+    <ErrorBoundary>
+      <Switch>
+        <Route path="/movies/:id">Movie</Route>
+        <Route path="/">
+          <MainRouter />
+        </Route>
+      </Switch>
+    </ErrorBoundary>
   );
 };
 
